a3a9c722936ed71ae0d5377d7cf047b536b6fe90
Jemmie Renard <jemmierenard17@gmail.com>
2022-04-11 13:13:56 +0700

    Add project files.

---
+++ PraktikumWeek8/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace PraktikumWeek8
+{
+    partial class FormHasilPertandingan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cBoxHome = new System.Windows.Forms.ComboBox();
+            this.cBoxLawan = new System.Windows.Forms.ComboBox();
+            this.lblVs = new System.Windows.Forms.Label();
+            this.lblManagHome = new System.Windows.Forms.Label();
+            this.lblCaptHome = new System.Windows.Forms.Label();
+            this.lblManagHomeIsi = new System.Windows.Forms.Label();
+            this.lblCaptHomeIsi = new System.Windows.Forms.Label();
+            this.lblCaptLawanIsi = new System.Windows.Forms.Label();
+            this.lblManagLawanIsi = new System.Windows.Forms.Label();
+            this.lblCaptLawan = new System.Windows.Forms.Label();
+            this.lblManagLawan = new System.Windows.Forms.Label();
+            this.lblCapacityIsi = new System.Windows.Forms.Label();
+            this.lblStadiumIsi = new System.Windows.Forms.Label();
+            this.lblCapacity = new System.Windows.Forms.Label();
+            this.lblStadium = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cBoxHome
+            // 
+            this.cBoxHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cBoxHome.FormattingEnabled = true;
+            this.cBoxHome.Location = new System.Drawing.Point(45, 46);
+            this.cBoxHome.Name = "cBoxHome";
+            this.cBoxHome.Size = new System.Drawing.Size(198, 28);
+            this.cBoxHome.TabIndex = 0;
+            this.cBoxHome.SelectedIndexChanged += new System.EventHandler(this.cBoxHome_SelectedIndexChanged);
+            // 
+            // cBoxLawan
+            // 
+            this.cBoxLawan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cBoxLawan.FormattingEnabled = true;
+            this.cBoxLawan.Location = new System.Drawing.Point(510, 46);
+            this.cBoxLawan.Name = "cBoxLawan";
+            this.cBoxLawan.Size = new System.Drawing.Size(198, 28);
+            this.cBoxLawan.TabIndex = 1;
+            this.cBoxLawan.SelectedIndexChanged += new System.EventHandler(this.cBoxLawan_SelectedIndexChanged);
+            // 
+            // lblVs
+            // 
+            this.lblVs.AutoSize = true;
+            this.lblVs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVs.Location = new System.Drawing.Point(360, 50);
+            this.lblVs.Name = "lblVs";
+            this.lblVs.Size = new System.Drawing.Size(31, 20);
+            this.lblVs.TabIndex = 2;
+            this.lblVs.Text = "VS";
+            // 
+            // lblManagHome
+            // 
+            this.lblManagHome.AutoSize = true;
+            this.lblManagHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManagHome.Location = new System.Drawing.Point(40, 109);
+            this.lblManagHome.Name = "lblManagHome";
+            this.lblManagHome.Size = new System.Drawing.Size(101, 25);
+            this.lblManagHome.TabIndex = 3;
+            this.lblManagHome.Text = "Manager :";
+            // 
+            // lblCaptHome
+            // 
+            this.lblCaptHome.AutoSize = true;
+            this.lblCaptHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaptHome.Location = new System.Drawing.Point(40, 170);
+            this.lblCaptHome.Name = "lblCaptHome";
+            this.lblCaptHome.Size = new System.Drawing.Size(91, 25);
+            this.lblCaptHome.TabIndex = 4;
+            this.lblCaptHome.Text = "Captain :";
+            // 
+            // lblManagHomeIsi
+            // 
+            this.lblManagHomeIsi.AutoSize = true;
+            this.lblManagHomeIsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManagHomeIsi.Location = new System.Drawing.Point(177, 109);
+            this.lblManagHomeIsi.Name = "lblManagHomeIsi";
+            this.lblManagHomeIsi.Size = new System.Drawing.Size(0, 25);
+            this.lblManagHomeIsi.TabIndex = 5;
+            // 
+            // lblCaptHomeIsi
+            // 
+            this.lblCaptHomeIsi.AutoSize = true;
+            this.lblCaptHomeIsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaptHomeIsi.Location = new System.Drawing.Point(177, 170);
+            this.lblCaptHomeIsi.Name = "lblCaptHomeIsi";
+            this.lblCaptHomeIsi.Size = new System.Drawing.Size(0, 25);
+            this.lblCaptHomeIsi.TabIndex = 6;
+            // 
+            // lblCaptLawanIsi
+            // 
+            this.lblCaptLawanIsi.AutoSize = true;
+            this.lblCaptLawanIsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaptLawanIsi.Location = new System.Drawing.Point(642, 170);
+            this.lblCaptLawanIsi.Name = "lblCaptLawanIsi";
+            this.lblCaptLawanIsi.Size = new System.Drawing.Size(0, 25);
+            this.lblCaptLawanIsi.TabIndex = 10;
+            // 
+            // lblManagLawanIsi
+            // 
+            this.lblManagLawanIsi.AutoSize = true;
+            this.lblManagLawanIsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManagLawanIsi.Location = new System.Drawing.Point(642, 109);
+            this.lblManagLawanIsi.Name = "lblManagLawanIsi";
+            this.lblManagLawanIsi.Size = new System.Drawing.Size(0, 25);
+            this.lblManagLawanIsi.TabIndex = 9;
+            // 
+            // lblCaptLawan
+            // 
+            this.lblCaptLawan.AutoSize = true;
+            this.lblCaptLawan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaptLawan.Location = new System.Drawing.Point(505, 170);
+            this.lblCaptLawan.Name = "lblCaptLawan";
+            this.lblCaptLawan.Size = new System.Drawing.Size(96, 25);
+            this.lblCaptLawan.TabIndex = 8;
+            this.lblCaptLawan.Text = "Captain : ";
+            // 
+            // lblManagLawan
+            // 
+            this.lblManagLawan.AutoSize = true;
+            this.lblManagLawan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManagLawan.Location = new System.Drawing.Point(505, 109);
+            this.lblManagLawan.Name = "lblManagLawan";
+            this.lblManagLawan.Size = new System.Drawing.Size(101, 25);
+            this.lblManagLawan.TabIndex = 7;
+            this.lblManagLawan.Text = "Manager :";
+            // 
+            // lblCapacityIsi
+            // 
+            this.lblCapacityIsi.AutoSize = true;
+            this.lblCapacityIsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCapacityIsi.Location = new System.Drawing.Point(408, 339);
+            this.lblCapacityIsi.Name = "lblCapacityIsi";
+            this.lblCapacityIsi.Size = new System.Drawing.Size(0, 25);
+            this.lblCapacityIsi.TabIndex = 14;
+            // 
+            // lblStadiumIsi
+            // 
+            this.lblStadiumIsi.AutoSize = true;
+            this.lblStadiumIsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStadiumIsi.Location = new System.Drawing.Point(408, 278);
+            this.lblStadiumIsi.Name = "lblStadiumIsi";
+            this.lblStadiumIsi.Size = new System.Drawing.Size(0, 25);
+            this.lblStadiumIsi.TabIndex = 13;
+            // 
+            // lblCapacity
+            // 
+            this.lblCapacity.AutoSize = true;
+            this.lblCapacity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCapacity.Location = new System.Drawing.Point(271, 339);
+            this.lblCapacity.Name = "lblCapacity";
+            this.lblCapacity.Size = new System.Drawing.Size(100, 25);
+            this.lblCapacity.TabIndex = 12;
+            this.lblCapacity.Text = "Capacity :";
+            // 
+            // lblStadium
+            // 
+            this.lblStadium.AutoSize = true;
+            this.lblStadium.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStadium.Location = new System.Drawing.Point(271, 278);
+            this.lblStadium.Name = "lblStadium";
+            this.lblStadium.Size = new System.Drawing.Size(95, 25);
+            this.lblStadium.TabIndex = 11;
+            this.lblStadium.Text = "Stadium :";
+            // 
+            // FormHasilPertandingan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCapacityIsi);
+            this.Controls.Add(this.lblStadiumIsi);
+            this.Controls.Add(this.lblCapacity);
+            this.Controls.Add(this.lblStadium);
+            this.Controls.Add(this.lblCaptLawanIsi);
+            this.Controls.Add(this.lblManagLawanIsi);
+            this.Controls.Add(this.lblCaptLawan);
+            this.Controls.Add(this.lblManagLawan);
+            this.Controls.Add(this.lblCaptHomeIsi);
+            this.Controls.Add(this.lblManagHomeIsi);
+            this.Controls.Add(this.lblCaptHome);
+            this.Controls.Add(this.lblManagHome);
+            this.Controls.Add(this.lblVs);
+            this.Controls.Add(this.cBoxLawan);
+            this.Controls.Add(this.cBoxHome);
+            this.Name = "FormHasilPertandingan";
+            this.Text = "Hasil Pertandingan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cBoxHome;
+        private System.Windows.Forms.ComboBox cBoxLawan;
+        private System.Windows.Forms.Label lblVs;
+        private System.Windows.Forms.Label lblManagHome;
+        private System.Windows.Forms.Label lblCaptHome;
+        private System.Windows.Forms.Label lblManagHomeIsi;
+        private System.Windows.Forms.Label lblCaptHomeIsi;
+        private System.Windows.Forms.Label lblCaptLawanIsi;
+        private System.Windows.Forms.Label lblManagLawanIsi;
+        private System.Windows.Forms.Label lblCaptLawan;
+        private System.Windows.Forms.Label lblManagLawan;
+        private System.Windows.Forms.Label lblCapacityIsi;
+        private System.Windows.Forms.Label lblStadiumIsi;
+        private System.Windows.Forms.Label lblCapacity;
+        private System.Windows.Forms.Label lblStadium;
+    }
+}
+
---
+++ PraktikumWeek8/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PraktikumWeek8
+{
+    public partial class FormHasilPertandingan : System.Windows.Forms.Form
+    {
+        public FormHasilPertandingan()
+        {
+            InitializeComponent();
+        }
+
+        public static string sqlConnection = "server=localhost;uid=root;pwd=;database=premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(sqlConnection); //sebagai data koneksi ke DBMSnya
+        public MySqlCommand sqlCommand; //memindahkan query ke database, query terjadi di dbmsnya
+        public static MySqlDataAdapter sqlAdapter; //hasil dari query akan disimpan di mysqldata
+        public string sqlQuery;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            { 
+                DataTable dtTeamHome = new DataTable();
+                DataTable dtTeamLawan = new DataTable();
+                sqlQuery = "SELECT team_id as `ID TEAM`, team_name as `Nama Tim` from team";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtTeamHome);
+                sqlQuery = "SELECT team_name as `Nama Tim`, team_id as `ID TEAM` FROM team";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtTeamLawan);
+                cBoxHome.DataSource = dtTeamHome;
+                cBoxHome.DisplayMember = "Nama Tim";
+                cBoxHome.ValueMember = "Nama Tim";
+                cBoxLawan.DataSource = dtTeamLawan;
+                cBoxLawan.DisplayMember = "Nama Tim";
+                cBoxLawan.ValueMember = "Nama Tim";
+            }
+            catch (Exception)
+            {
+
+               
+            }
+        }
+
+        private void cBoxHome_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dtTim = new DataTable();
+                ////mulai dari sini
+                sqlQuery = "select t.team_name as 'namaTim' , p.player_name , m.manager_name, t.home_stadium, t.capacity from team t, manager m, player p WHERE t.manager_id = m.manager_id and t.captain_id = p.player_id and team_name = '" + cBoxHome.SelectedValue + "'";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtTim);
+                lblManagHomeIsi.Text = dtTim.Rows[0][2].ToString();
+                lblCaptHomeIsi.Text = dtTim.Rows[0][1].ToString();
+                lblStadiumIsi.Text = dtTim.Rows[0]["home_stadium"].ToString();
+                lblCapacityIsi.Text = dtTim.Rows[0]["capacity"].ToString();
+                ////sampai sini
+                
+            }
+            catch (Exception)
+            {
+
+                
+            }
+          
+        }
+
+        private void cBoxLawan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dtTim = new DataTable();
+                ////mulai dari sini
+                sqlQuery = "select t.team_name as 'namaTim' , p.player_name , m.manager_name, t.home_stadium, t.capacity from team t, manager m, player p WHERE t.manager_id = m.manager_id and t.captain_id = p.player_id and team_name = '" + cBoxLawan.SelectedValue + "'";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtTim);
+                lblManagLawanIsi.Text = dtTim.Rows[0][2].ToString();
+                lblCaptLawanIsi.Text = dtTim.Rows[0][1].ToString();
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+    }
+}
